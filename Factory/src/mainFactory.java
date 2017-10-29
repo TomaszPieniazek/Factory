24e0fe487050571150c7cadf3c960c0e5afad888
Tomasz Pieniążek <32618181+TomaszPieniazek@users.noreply.github.com>
2017-10-29 16:10:08 +0100

    testing

--- Factory/src/mainFactory.java
+++ Factory/src/mainFactory.java
@@ -11,9 +11,15 @@ public class mainFactory {
 		System.out.print(f1+" "+f2+" \n");
 		
 		f1.setAge(10);
-		f1.setCity("LODZ");
+		f1.setCity("Spychów");
 		f1.setName("Jurand");
-		System.out.print(f1.getName()+" "+f1.getAge()+" "+f1.getCity());
+		System.out.print("\n"+f1.getName()+" "+f1.getAge()+" "+f1.getCity());
+		
+		f2.setAge(22);
+		f2.setCity("Szczytno");
+		f2.setName("Zygfryd de Lowe");
+		System.out.print("\n"+f2.getName()+" "+f2.getAge()+" "+f2.getCity());
+		System.out.print("\n"+f1.getName()+" "+f1.getAge()+" "+f1.getCity());
 		System.out.print("\n"+f1+" "+f2+" \n");
 		
 		f1=DAOFactoryProvider.setType(EFactory.XML);
