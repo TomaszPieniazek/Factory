d07a99f86c01b6408b035862d485cb8dddd18462
Tomasz Pieniążek <32618181+TomaszPieniazek@users.noreply.github.com>
2017-10-29 11:56:37 +0100

    main changes

--- Factory/src/mainFactory.java
+++ Factory/src/mainFactory.java
@@ -1,4 +1,3 @@
-import domain.Person;
 import factory.DAOFactoryProvider;
 import factory.EFactory;
 import factory.IFactory;
@@ -6,23 +5,23 @@ import factory.IFactory;
 public class mainFactory {
 
 	public static void main(String[] args) {
-		IFactory f2=DAOFactoryProvider.setType(EFactory.XML);
-		
+	
 		IFactory f1= DAOFactoryProvider.setType(EFactory.DB);
+		
+		
 		f1.setAge(10);
 		f1.setCity("LODZ");
 		f1.setName("Jurand");
 		System.out.print(f1.getName()+" "+f1.getAge()+" "+f1.getCity());
-		System.out.print("\n"+f1+"\n"+f2);
+
 		
 		f1=DAOFactoryProvider.setType(EFactory.XML);
 		f1.setAge(10);
 		f1.setCity("LODZ");
 		f1.setName("Jurand");
 		System.out.print("\n"+f1.getName()+" "+f1.getAge()+" "+f1.getCity());
-		f2=DAOFactoryProvider.setType(EFactory.XML);
 		
-		System.out.print("\n"+f1+"\n"+f2);
+				
 		
 	}
 
